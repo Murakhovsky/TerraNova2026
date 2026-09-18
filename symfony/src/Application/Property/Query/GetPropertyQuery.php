35a9c34cd61257e00ef998d7d1d7d7bb27ce8c14
Murakhovsky <41971803+Murakhovsky@users.noreply.github.com>
2026-09-18 22:34:34 +0300

    Wave 9: add Symfony Property and RealEstate CQRS

---
+++ symfony/src/Application/Property/Query/GetPropertyQuery.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\Property\Query;
+
+use Kernel\Application\Query\QueryInterface;
+use Kernel\Shared\Domain\OrganizationId;
+
+final readonly class GetPropertyQuery implements QueryInterface
+{
+    public function __construct(public OrganizationId $organizationId, public string $reference) {}
+}
