58ffb363dffc80fc468a003bf1e841bac438df67
Murakhovsky <41971803+Murakhovsky@users.noreply.github.com>
2026-09-20 00:10:26 +0300

    refactor(visualization): bind Cytoscape mapper to Kernel port

--- app/Infrastructure/Visualization/Cytoscape/CytoscapeGraphMapper.php
+++ app/Infrastructure/Visualization/Cytoscape/CytoscapeGraphMapper.php
@@ -5,6 +5,7 @@ namespace Infrastructure\Visualization\Cytoscape;
 
 use Kernel\Visualization\Graph\Edge;
 use Kernel\Visualization\Graph\Graph;
+use Kernel\Visualization\Graph\GraphPayloadMapperInterface;
 use Kernel\Visualization\Graph\Group;
 use Kernel\Visualization\Graph\Node;
 
