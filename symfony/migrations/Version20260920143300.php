5156bfccdb7ab3c3beabf761794ab6eb17977553
Murakhovsky <41971803+Murakhovsky@users.noreply.github.com>
2026-09-20 17:49:02 +0300

    fix: use DBAL 4 MySQL platform contract

--- symfony/migrations/Version20260920143300.php
+++ symfony/migrations/Version20260920143300.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
+use Doctrine\DBAL\Platforms\AbstractMySQLPlatform;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -16,7 +17,7 @@ final class Version20260920143300 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->abortIf(
-            $this->connection->getDatabasePlatform()->getName() !== 'MySQL',
+            !$this->connection->getDatabasePlatform() instanceof AbstractMySQLPlatform,
             'This migration can only be executed safely on MySQL.',
         );
 
