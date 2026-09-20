f7cc96986f23734d8e6200449b0e1d940e22e890
Murakhovsky <41971803+Murakhovsky@users.noreply.github.com>
2026-09-20 07:32:12 +0300

    feat(web): cut Spatial Web ingress to Symfony

--- deploy/configure-company-os-http.sh
+++ deploy/configure-company-os-http.sh
@@ -123,6 +123,19 @@ server {
         proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_set_header X-Forwarded-Host \$host;
     }
+    # Spatial Web workspace and public viewer are canonical on Symfony.
+    location ^~ /spatial/ {
+        client_max_body_size 220m;
+        proxy_pass http://$SYMFONY_UPSTREAM;
+        proxy_http_version 1.1;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-Host \$host;
+        proxy_read_timeout 300s;
+        proxy_send_timeout 300s;
+    }
     # Canonical COS business/control-plane APIs are served by Symfony.
     location ^~ /api/v1/ {
         proxy_pass http://$SYMFONY_UPSTREAM;
