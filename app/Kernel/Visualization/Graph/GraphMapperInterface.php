f6a49d9cd3e726be4e7fd2a128427e7f61223002
Murakhovsky <41971803+Murakhovsky@users.noreply.github.com>
2026-09-20 08:21:23 +0300

    refactor(visualization): add renderer-neutral graph mapper port

---
+++ app/Kernel/Visualization/Graph/GraphMapperInterface.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Kernel\Visualization\Graph;
+
+interface GraphMapperInterface
+{
+    /** @return array<string,mixed> */
+    public function map(Graph $graph): array;
+}
