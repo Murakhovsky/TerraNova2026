cd82bb8aad55876b86a486db813d0cd771c1ca49
Murakhovsky <41971803+Murakhovsky@users.noreply.github.com>
2026-09-21 10:07:28 +0300

    Wave 12.5: use stable diagnosticNavigationContributor manifest service id

--- app/Domains/Diagnostic/module.php
+++ app/Domains/Diagnostic/module.php
@@ -20,9 +20,9 @@ return [
             'event.consumers' => [
                 'diagnosticActionOutcomeHandler',
             ],
-            'web.navigation' => ['diagnosticWebProvider'],
-            'web.commands' => ['diagnosticWebProvider'],
-            'web.workspace' => ['diagnosticWebProvider'],
+            'web.navigation' => ['diagnosticNavigationContributor'],
+            'web.commands' => ['diagnosticNavigationContributor'],
+            'web.workspace' => ['diagnosticNavigationContributor'],
         ],
         'migration_files' => [
             'app/migrations/20260914_000049_diagnostic_runtime_v060.sql',
