9ceb94218380d4b4a9d032a9864778b1d2438d2d
Murakhovsky <41971803+Murakhovsky@users.noreply.github.com>
2026-09-21 22:26:11 +0300

    Wave 12.24: restore symfony/assets/app.js

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -1,6 +1,9 @@
 import './stimulus_bootstrap.js';
 import './pwa_runtime.js';
+import { initWebTelemetry } from './web_telemetry.js';
 import '@hotwired/turbo';
 import 'bootstrap';
 
 document.documentElement.dataset.cosExperienceRuntime = 'assetmapper';
+
+initWebTelemetry();
