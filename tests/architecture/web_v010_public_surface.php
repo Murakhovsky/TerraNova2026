c33e5b98729e342b772f4d1624133c799a498d3e
Murakhovsky <41971803+Murakhovsky@users.noreply.github.com>
2026-09-22 23:43:37 +0300

    Final UI freeze audit: guard restored public page routes

--- tests/architecture/web_v010_public_surface.php
+++ tests/architecture/web_v010_public_surface.php
@@ -42,9 +42,14 @@ foreach ([
 
 $contentController = $read('symfony/src/Web/Content/PublicContentPageController.php');
 foreach ([
+    "public function page(Request \$request, string \$slug): Response",
     "public function blog(Request \$request): Response",
     "public function article(Request \$request, string \$slug): Response",
     "public function guide(Request \$request, string \$slug): Response",
+    "'page/show'",
+    "PublicPageCatalog",
+    "SalesWriteServiceFactoryInterface",
+    "receivePublicLead",
     "'blog/index'",
     "'blog/show'",
     "'blog/landing'",
@@ -75,6 +80,9 @@ $routes = $read('symfony/config/routes.yaml');
 foreach ([
     'path: /',
     'HomePageController',
+    'path: /{slug}',
+    'PublicContentPageController::page',
+    "slug: 'terra-nova|agency|services|partners|team|cases|vacancies|contacts|it|art'",
     'path: /blog',
     'PublicContentPageController::blog',
     'path: /blog/{slug}',
